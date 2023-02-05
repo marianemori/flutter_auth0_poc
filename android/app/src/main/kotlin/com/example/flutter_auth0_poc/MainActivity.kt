97a06697a4206ec270a9e245db9734d2aa5372d5
Mariane <mariane.mori@grupocard.com.br>
2023-02-05 15:28:19 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_auth0_poc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_auth0_poc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Auth0'),
+        ),
+        body: const Center(child: Text('Flutter Auth0')));
+  }
+}
